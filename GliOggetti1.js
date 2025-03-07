fbbf5998ccf7ff353b34b3b10188ce18101f127a
JustRick <Riccardodicaccamo@gmail.com>
2025-03-07 19:05:18 +0100

    oggetti

---
+++ GliOggetti1.js
@@ -0,0 +1,10 @@
+//Crea un oggetto che rappresenti un'auto con almeno tre proprietà (marca, modello, anno) e stampalo in console. Esercizio N. 1
+
+let auto = {
+    marca: "Alfa Romeo",
+    modello: "6C",
+    anno: 2025
+};
+
+console.log(auto)
+
